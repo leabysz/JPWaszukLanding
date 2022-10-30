1633fb63b85518014c90937547eef9f2d2100ac4
Bruno Paulette <brunopaulettecurbelo@gmail.com>
2022-10-30 10:10:43 -0300

    New NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -16,47 +16,39 @@ function NavBar() {
       />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto" style={{ width: '100%' }}>
-          <Nav.Link>
-            <Link to="about" smooth duration={1000}>
-              `{t('header.btnAbout')}`
-            </Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link to="projects" smooth duration={1000}>
-              {t('header.btnMyPractice')}
-            </Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link to="ACT" smooth duration={1000}>
-              {t('header.btnACT')}
-            </Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link to="contact" smooth duration={1000}>
-              {t('header.btnContact')}
-            </Link>
-          </Nav.Link>
-          <Nav.Link>
-            <nav style={{ display: 'inline-flex' }}>
-              <button
-                type="button"
-                onClick={() => i18n.changeLanguage('en')}
-                className="cta-btn cta-btn--resume"
-                style={{ backgroundImage: 'linear-gradient(135deg, #02aab0 0%, #00cdac 100%)' }}
-              >
-                EN
-              </button>
-              <button
-                type="button"
-                onClick={() => i18n.changeLanguage('es')}
-                className="cta-btn cta-btn--resume"
-                style={{ backgroundImage: 'linear-gradient(135deg, #02aab0 0%, #00cdac 100%)' }}
-              >
-                ES
-              </button>
-            </nav>
-          </Nav.Link>
+          <Link to="projects" smooth duration={1000} className="nav-link">
+            {t('header.btnAbout')}
+          </Link>
+          <Link to="projects" smooth duration={1000} className="nav-link">
+            {t('header.btnMyPractice')}
+          </Link>
+          <Link to="ACT" smooth duration={1000} className="nav-link">
+            {t('header.btnACT')}
+          </Link>
+          <Link to="contact" smooth duration={1000} className="nav-link">
+            {t('header.btnContact')}
+          </Link>
         </Nav>
+        <Nav.Link style={{ widt: '100%' }}>
+          <nav style={{ display: 'inline-flex' }}>
+            <button
+              type="button"
+              onClick={() => i18n.changeLanguage('en')}
+              className="cta-btn cta-btn--resume"
+              style={{ backgroundImage: 'linear-gradient(135deg, #02aab0 0%, #00cdac 100%)' }}
+            >
+              EN
+            </button>
+            <button
+              type="button"
+              onClick={() => i18n.changeLanguage('es')}
+              className="cta-btn cta-btn--resume"
+              style={{ backgroundImage: 'linear-gradient(135deg, #02aab0 0%, #00cdac 100%)' }}
+            >
+              ES
+            </button>
+          </nav>
+        </Nav.Link>
       </Navbar.Collapse>
     </Navbar>
   );
