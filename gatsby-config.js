028a54f49200341070dc1f13ce839b7bdc3f93e8
Bruno Paulette <brunopaulettecurbelo@gmail.com>
2023-07-28 18:42:34 -0300

    mobile chrome color

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         short_name: `Juan Pablo`,
         start_url: `/`,
         background_color: `#fff`,
-        theme_color: `#02aab0`,
+        theme_color: `#9C6D43`,
         display: `standalone`,
         icon: 'src/images/favicon.png',
       },
