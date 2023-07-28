5e674c6efc2029f9658efdced22ec33016712791
Bruno Paulette <brunopaulettecurbelo@gmail.com>
2023-07-28 18:47:20 -0300

    hero pic new style

--- src/components/FluidImg/fluidimg.jsx
+++ src/components/FluidImg/fluidimg.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Image from 'react-bootstrap/Image';
 
 const FluidImage = ({ src, alt }) => {
-  return <Image fluid className="fluid-image" src={src} alt={alt} width="600em" />;
+  return <Image fluid className="rounded shadow-lg" src={src} alt={alt} width="600em" />;
 };
 
 export default FluidImage;
