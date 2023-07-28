a05c041460c18def82afa25004261cc43a184707
Bruno Paulette <brunopaulettecurbelo@gmail.com>
2023-07-28 08:54:03 -0300

    Hero desktop img

--- src/components/FluidImg/fluidimg.jsx
+++ src/components/FluidImg/fluidimg.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Image from 'react-bootstrap/Image';
 
 const FluidImage = ({ src, alt }) => {
-  return <Image fluid className="fluid-image" src={src} alt={alt} width="60%" />;
+  return <Image fluid className="fluid-image" src={src} alt={alt} width="600em" />;
 };
 
 export default FluidImage;
